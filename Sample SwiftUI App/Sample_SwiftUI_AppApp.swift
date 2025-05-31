6540b6525993a30e6eae78dbc80225408755cca8
Navin Rai <rai.navinrai20@gmail.com>
2025-05-31 23:27:16 +0530

    Initial Commit

---
+++ Sample SwiftUI App/Sample_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_SwiftUI_AppApp.swift
+//  Sample SwiftUI App
+//
+//  Created by Navin Rai on 31/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Sample_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
